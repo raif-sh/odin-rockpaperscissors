0ce69f9c2bf95e555c053c5be8b3c1086b3ca9b5
Raif Shareef <raif.sh94@gmail.com>
2025-01-25 02:02:46 +0800

    adding some comments for computer and human functions

--- game.js
+++ game.js
@@ -1,28 +1,35 @@
-// function that randomly returns 'rock', 'paper' or 'scissors'.
-
+// Create a function that randomly returns 'rock', 'paper' or 'scissors'.
 function getComputerChoice() {
-    // generate a random number in range of 0 to 2
+    // Generate a random number between 0 to 1
     let randomNum = Math.random();
     let choice = ""
+
+    // Create a variable as a control to create three segments
     let divider = 1 / 3
 
-    // return one of the options based on the number
+    // Check whether random number falls into the first sement
     if (randomNum <= divider) {
         choice = "Rock";
+    // Check if random number is in the middle segment
     } else if (randomNum < (divider * 2) && randomNum > divider) {
         choice = "Paper";
+    // If both of the conditions are not met, assign value for the rest
     } else {
         choice = "Scissors";
     }
     return choice;
 }
 
+// Create a variable to lock in computer choice for this round
 let computerChoice = getComputerChoice();
 
+// Create a function that gets use input for their choice
 function getHumanChoice() {
+    // Creating a variable to prompt user
     input = parseInt(prompt("Enter 1 (for rock), 2 (for paper) or 3 (for scissors): "))
     choice = ""
-    
+
+    // Check if the input matches three options and assign the value
     if (input === 1) {
         choice = "Rock";
     } else if (input === 2) {
@@ -35,5 +42,5 @@ function getHumanChoice() {
     return choice;
 }
 
+// Creating a variable to track user choice for current round
 let humanChoice = getHumanChoice();
-console.log(humanChoice)
