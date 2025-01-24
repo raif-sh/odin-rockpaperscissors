a616a476d62922226e1d34a0580418e4cd5ad472
Raif Shareef <raif.sh94@gmail.com>
2025-01-25 01:26:03 +0800

    Created function to generate computer choice

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("game on")
+// function that randomly returns 'rock', 'paper' or 'scissors'.
+
+function getComputerChoice() {
+    // generate a random number in range of 0 to 2
+    let randomNum = Math.random();
+    let choice = ""
+    let divider = 1 / 3
+
+    // return one of the options based on the number
+    if (randomNum <= divider) {
+        choice = "Rock";
+    } else if (randomNum < (divider * 2) && randomNum > divider) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    return choice;
+}
+
+let computerChoice = getComputerChoice();
